6eb8c51065e93d8e8aaa9069fa79a920496ea305
RostisLAVE01 <moskalets_03@mail.ru>
2024-02-09 00:56:48 +0300

    adding comments

--- LR1.pl
+++ LR1.pl
@@ -62,39 +62,57 @@ parent(vlada,velerad).
 
 %номер1
 
+%men(?X)
 men():- man(X), print(X), nl, fail.
+%woman(?X)
 women():- woman(X), print(X), nl, fail.
+%children(+X)
 children(X):- parent(X,Y), print(Y), nl, fail.
 
+%mother(+X,+Y)
 mother(X,Y):- woman(X), parent(X,Y).
+%mother(+X)
 mother(X):- mother(Y,X), print(Y), nl, fail.
 
-
+%brother(+X,+Y)
 brother(X,Y):- man(X), parent(Z,X), man(Z), woman(Y), parent(Z,Y).
+%brother(+X)
 brother(X):- parent(Z,X), man(Z), parent(Z,Y), man(Y), not(man(X) = man(Y)), print(Y), nl, fail.
 
+%b_s(+X,+Y)
 b_s(X,Y):- parent(Z,X), man(Z), parent(Z,Y).
+%b_s(+X)
 b_s(X):- parent(Z,X), man(Z), parent(Z,Y), not(man(X) = man(Y); woman(X) = woman(Y)), print(Y), nl, fail.
 
 
 %номер2
 
+%father(+X,+Y)
 father(X,Y):- parent(X,Y),  man(X).
+%father(+X)
 father(X):- father(Y,X), print(Y), nl, fail.
 
+%sister(+X,+Y)
 sister(X,Y):- woman(X), parent(Z,X), man(Z), parent(Z,Y).
-sister(X,Y):- woman(X), parent(Z,X), man(Z), parent(Z,Y), woman(Y), not(woman(X) = woman(Y)), print(Y), nl, fail.
+%sister(+X)
+sister(X):- woman(X), parent(Z,X), man(Z), parent(Z,Y), woman(Y), not(woman(X) = woman(Y)), print(Y), nl, fail.
 
 %номер3
 
+%grand_pa(+X,+Y)
 grand_pa(X,Y):- man(X), parent(X,Y), parent(Y,Z).
+%grand_pa_fk(+X,+Y)
 grand_pa_fk(X,Y):- father(X,Y),parent(Y,Z).
+%grand_pa(+X)
 grand_pa(X):-  parent(Z,X), parent(Y,Z), man(Y), print(Y), nl, fail.
 
+%grand_pa_and_son(+X,+Y)
 grand_pa_and_son(X,Y):- parent(X,Z), man(X), parent(Z,Y), man(Y) ; parent(Z,X), man(X), parent(Y,Z), man(Y).
 
 %ДЯДЯ!!!!!
+%uncle_fk(+X,+Y)
 uncle(X,Y):- parent(Z,Y), man(Z), parent(S,Z), man(S), parent(S,X), man(X).
+%uncle_fk(+X,+Y)
 uncle_fk(X,Y):- father(Z,Y), father(S,Z), father(S,X).
 %uncle(+X)
 uncle(X):-parent(Z,X), man(Z), parent(S,Z), man(S), parent(S,Y), man(Y), not(man(Z) = man(Y)),print(Y),nl,fail.
