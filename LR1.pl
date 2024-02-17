7e308599d97fbe9f065698484673a5d36b0e5fb4
RostisLAVE01 <moskalets_03@mail.ru>
2024-02-17 19:44:59 +0300

    fixing errors in predicates

--- LR1.pl
+++ LR1.pl
@@ -86,6 +86,10 @@ b_s(X):- parent(Z,X), man(Z), parent(Z,Y), not(man(X) = man(Y); woman(X) = woman
 
 
 %номер2
+%Построить предикат father(X, Y), который проверяет, является ли X отцом Y.
+%Построить предикат, father(X), который выводит отца X.
+%Построить предикат sister(X, Y), который проверяет, является ли X сестрой Y.
+%Построить предикат sisters(X), который выводит всех сестер X.
 
 %father(+X,+Y)
 father(X,Y):- parent(X,Y),  man(X).
@@ -93,29 +97,35 @@ father(X,Y):- parent(X,Y),  man(X).
 father(X):- father(Y,X), print(Y), nl, fail.
 
 %sister(+X,+Y)
-sister(X,Y):- woman(X), parent(Z,X), man(Z), parent(Z,Y).
+sister(X,Y):- woman(X), parent(Z,X), man(Z), parent(Z,Y), not(woman(X) = woman(Y)).
 %sister(+X)
 sister(X):- woman(X), parent(Z,X), man(Z), parent(Z,Y), woman(Y), not(woman(X) = woman(Y)), print(Y), nl, fail.
 
 %номер3
+%Построить предикат grand_pa(X, Y), который проверяет, является ли X дедушкой Y.
+%Построить предикат grand_pas(X), который выводит всех дедушек X.
+%Построить предикат grand_pa_and_son(X,Y), который проверяет, являются ли X и Y
+%дедушкой и внуком или внуком и дедушкой.
+%Построить предикат, который проверяет, является ли X дядей Y. Построить
+%предикат, который выводит всех дядей X.
 
 %grand_pa(+X,+Y)
-grand_pa(X,Y):- man(X), parent(X,Y), parent(Y,Z).
+grand_pa(X,Y):- man(X), parent(X,Z), parent(Z,Y).
 %grand_pa_fk(+X,+Y)
 grand_pa_fk(X,Y):- father(X,Y),parent(Y,Z).
 %grand_pa(+X)
-grand_pa(X):-  parent(Z,X), parent(Y,Z), man(Y), print(Y), nl, fail.
+grand_pas(X):-  parent(Z,X), parent(Y,Z), man(Y), print(Y), nl, fail.
 
 %grand_pa_and_son(+X,+Y)
 grand_pa_and_son(X,Y):- parent(X,Z), man(X), parent(Z,Y), man(Y) ; parent(Z,X), man(X), parent(Y,Z), man(Y).
 
 %ДЯДЯ!!!!!
 %uncle_fk(+X,+Y)
-uncle(X,Y):- parent(Z,Y), man(Z), parent(S,Z), man(S), parent(S,X), man(X).
+uncle(X,Y):- parent(Z,Y), parent(S,Z), man(S), parent(S,X), man(X).
 %uncle_fk(+X,+Y)
-uncle_fk(X,Y):- father(Z,Y), father(S,Z), father(S,X).
+uncle_fk(X,Y):- parent(Z,Y), father(S,Z), father(S,X).
 %uncle(+X)
-uncle(X):-parent(Z,X), man(Z), parent(S,Z), man(S), parent(S,Y), man(Y), not(man(Z) = man(Y)),print(Y),nl,fail.
+uncle(X):-parent(Z,X), parent(S,Z), man(S), parent(S,Y), man(Y), not(man(Z) = man(Y)),print(Y),nl,fail.
 %дописать 3 задание с функциями (это нужно по условию!!!!)
 
 
@@ -446,3 +456,12 @@ desc(Prompt) :-
 	assert(asked(Prompt, Reply))
     ),
     Reply = y.
+
+
+
+
+
+
+%номер7  
+
+
