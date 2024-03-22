9a32ee5b1ed1e0206f471989ab6c6d1d350e70f5
RostisLAVE01 <moskalets_03@mail.ru>
2024-03-22 11:27:49 +0300

    lab2_№4_didn't_understand

--- logic_problem.pl
+++ logic_problem.pl
@@ -27,22 +27,3 @@ main:-People=[_,_,_,_],
     write('Позировал - '),write(WHY1),nl,
     write('Сидел - '),write(WHY4),nl,
     write('Сидел - '),write(WHY3).
-
-
-    %in_list
-in_list([El|_],El).
-in_list([_|T],El):-in_list(T,El).
-
-pr_ein:-Workers=[_,_,_,_],
-
-        in_list(Workers,[_,_,3,semenov]),
-        in_list(Workers,[_,true,_,borisov]),
-        in_list(Workers,[_,_,_,ivanov]),
-
-
-		    in_list(Workers,[tokar,_,2,WHO1]),
-		    in_list(Workers,[slesar,false,1,WHO2]),
-        in_list(Workers,[svarshik,_,_,WHO3]),
-		    write('Tokar - '),write(WHO1),nl,
-        write('Slesar - '),write(WHO2),nl,
-        write('Svarshik - '),write(WHO3).
